88272d9963d1633fc30cad24872c19c15e5ad44c
Ben B <bburdette@gmail.com>
2015-09-28 13:35:49 -0600

    squish together demo code from buttons and etc

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,9 +1,44 @@
 module Main where
 
+import Effects exposing (Never)
+import SpinSquarePair exposing (init, update, view)
+import StartApp
+import Task exposing (Task)
+import WebSocket exposing (WebSocket)
+import Keyboard
+
+socket : Task x WebSocket
+socket = WebSocket.create "ws://localhost:1234"
+
+listen : Signal.Mailbox String
+listen = Signal.mailbox ""
+
+inputKeyboard : Signal String
+inputKeyboard = Signal.map (\c -> toString c) Keyboard.presses
+
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+main =
+  app.html
+
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
+
+
+{-
+module Main where
+
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Signal exposing (Signal)
-import WebSocket exposing (WebSocket)
 import Task exposing (Task)
 import Keyboard
 import Char
@@ -16,9 +51,6 @@ socket = WebSocket.create "ws://localhost:1234"
 --         inputKeyboard listen.signal
 main = Signal.map3 (\a b c -> show ("Sending: " ++ a ++ ", Receiving: " ++ b ++ ", Connected: " ++ c))
          inputKeyboard listen.signal (Signal.map toString connected.signal)
-
-listen : Signal.Mailbox String
-listen = Signal.mailbox ""
 
 port listening : Task x ()
 port listening = socket `Task.andThen` WebSocket.listen listen.address
@@ -38,4 +70,4 @@ port sending = Signal.map send inputKeyboard
 inputKeyboard : Signal String
 -- inputKeyboard = Signal.map (\c -> Char.fromCode c |> String.fromChar) Keyboard.presses
 inputKeyboard = Signal.map (\c -> toString c) Keyboard.presses
-
+-}

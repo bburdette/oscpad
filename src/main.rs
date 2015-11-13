28ee6f0358e6f9b94fed7fcba1776133a17a938f
Ben B <bburdette@gmail.com>
2015-11-13 08:32:53 -0700

    echo messages to other clients but not to the sender.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,12 @@ use std::io::Read;
 use std::string::*;
 use std::collections::BTreeMap;
 
+use std::net::SocketAddr;
+
 use websocket::{Server, Message, Sender, Receiver};
 use websocket::header::WebSocketProtocol;
+use websocket::stream::WebSocketStream;
+use websocket::server::sender;
 
 extern crate iron;
 
@@ -130,6 +134,75 @@ fn startserver(config: Value)
     }).http(&ipnport[..]).unwrap();
 }
 
+type sendBlah = Arc<Mutex<sender::Sender<WebSocketStream>>>;
+
+
+#[derive(Clone)]
+struct Broadcaster {
+    tvs : Arc<Mutex<Vec<sendBlah >>>
+}
+
+fn test (sa: &SocketAddr) -> bool { 
+  match sa {
+    &SocketAddr::V4(sav4) => true,
+    _ => false,
+  }
+}
+
+fn mysockeq(saleft: &SocketAddr, saright: &SocketAddr) -> bool {
+  match (saleft, saright) {
+    (&SocketAddr::V4(l),&SocketAddr::V4(r)) => l == r,
+    (&SocketAddr::V6(l),&SocketAddr::V6(r)) => l == r,
+    _ => false,
+  }
+}
+
+
+impl Broadcaster {
+    fn new() -> Broadcaster {
+        Broadcaster {
+            tvs : Arc::new(Mutex::new(Vec::new()))
+        }
+    }
+
+    fn register(&mut self, sender : sendBlah) {
+        let mut tvs = self.tvs.lock().unwrap();
+
+        tvs.push(sender);
+    }
+
+    fn broadcast(&mut self, msg : Message) {
+        let mut tvs = self.tvs.lock().unwrap();
+
+        for tv in tvs.iter_mut() {
+            let mut tvsend = tv.lock().unwrap();
+            match tvsend.send_message(msg.clone()) {
+                Err(e) => {},
+                _ => {}
+            }
+        }
+    }
+   
+    fn broadcast_others(&mut self, sa: &SocketAddr , msg : Message) {
+      let mut tvs = self.tvs.lock().unwrap();
+
+      for tv in tvs.iter_mut() {
+        let mut tvsend = tv.lock().unwrap();
+        match tvsend.get_mut().peer_addr() { 
+          Ok(sa_send) => 
+            if !mysockeq(sa,&sa_send) {
+              match tvsend.send_message(msg.clone()) {
+                Err(e) => {},
+                _ => {}
+              }
+            },
+          Err(e) => {},
+        }
+      }
+    }
+}
+
+
 fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) {
   let ipnport = ipaddr + ":1234";
 	let server = Server::bind(&ipnport[..]).unwrap();
@@ -142,9 +215,13 @@ fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) 
     
     let scm = shareblah.clone();
 
+    let mut broadcaster = Broadcaster::new();
+
     thread::spawn(move || {
-			let request = connection.unwrap().read_request().unwrap(); // Get the request
-			let headers = request.headers.clone(); // Keep the headers so we can check them
+      // Get the request
+			let request = connection.unwrap().read_request().unwrap(); 
+      // Keep the headers so we can check them
+			let headers = request.headers.clone(); 
 			
 			request.validate().unwrap(); // Validate the request
 			
@@ -170,6 +247,10 @@ fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) 
 			client.send_message(message.clone()).unwrap();
 			
 			let (mut sender, mut receiver) = client.split();
+
+      let sendmeh = Arc::new(Mutex::new(sender));
+      
+      broadcaster.register(sendmeh.clone());      
 			
 			for message in receiver.incoming_messages() {
 				let message = message.unwrap();
@@ -180,6 +261,7 @@ fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) 
 				match message {
 					Message::Close(_) => {
 						let message = Message::Close(None);
+            let mut sender = sendmeh.lock().unwrap();
 						sender.send_message(message).unwrap();
 						println!("Client {} disconnected", ip);
 						return;
@@ -187,6 +269,7 @@ fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) 
 					Message::Ping(data) => {
             println!("Message::Ping(data)");
 						let message = Message::Pong(data);
+            let mut sender = sendmeh.lock().unwrap();
 						sender.send_message(message).unwrap();
 					}
           Message::Text(texxt) => {
@@ -199,7 +282,8 @@ fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) 
                 match cntrl {
                   Some(x) => {
                     (*x).update(&updmsg);
-                    println!("some x: {:?}", *x)
+                    println!("some x: {:?}", *x);
+                    broadcaster.broadcast_others(&ip, Message::Text(texxt))
                     },
                   None => println!("none"),
                   }
@@ -209,6 +293,7 @@ fn winsockets_main(ipaddr: String, aSConfig: String, cm: controls::controlMap ) 
             },
 					_ => { 
             println!("sending replyy");
+            let mut sender = sendmeh.lock().unwrap();
             sender.send_message(replyy).unwrap()
             }
 				}
